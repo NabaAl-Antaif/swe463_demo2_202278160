39e40fc56cc46bf67cca7bb561734fba79618232
NabaAl-Antaif <s202278160@kfupm.edu.sa>
2026-09-01 11:22:14 +0300

    task1.2

--- dart_lab02/lib/main.dart
+++ dart_lab02/lib/main.dart
@@ -1,11 +1,4 @@
 void main() {
-  int age = 22;
-  double height = 1.60;
-  bool isStudent = true;
-  String name = 'Naba';
-  List<int> scores = [90, 80, 100];
-
-  print('$name is $age years old and $height meters tall.');
-  print('Student: $isStudent');
-  print('Scores: $scores');
+  var city = 'Qatif';
+  print('City: $city');
 }
