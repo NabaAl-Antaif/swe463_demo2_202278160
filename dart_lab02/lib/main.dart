2584aaa21e1e0d6316422f6f221f33d8b1725ac6
NabaAl-Antaif <s202278160@kfupm.edu.sa>
2026-09-01 18:49:29 +0300

    task5.3

--- dart_lab02/lib/main.dart
+++ dart_lab02/lib/main.dart
@@ -135,50 +135,75 @@
 // }
 
 //Task 5.2
-abstract class Post {
-  final String author;
-  Post(this.author);
-  void render();
-}
+// abstract class Post {
+//   final String author;
+//   Post(this.author);
+//   void render();
+// }
 
-class TextPost extends Post {
-  final String text;
-  TextPost(String author, this.text) : super(author);
+// class TextPost extends Post {
+//   final String text;
+//   TextPost(String author, this.text) : super(author);
 
-  @override
-  void render() {
-    print('Text post by $author: "$text"');
+//   @override
+//   void render() {
+//     print('Text post by $author: "$text"');
+//   }
+// }
+
+// class ImagePost extends Post {
+//   final String imageUrl;
+//   ImagePost(String author, this.imageUrl) : super(author);
+
+//   @override
+//   void render() {
+//     print('Image post by $author at $imageUrl');
+//   }
+// }
+
+// class VideoPost extends Post {
+//   final String videoUrl;
+//   VideoPost(String author, this.videoUrl) : super(author);
+
+//   @override
+//   void render() {
+//     print('Video post by $author at $videoUrl');
+//   }
+// }
+
+// void main() {
+//   List<Post> feed = [
+//     TextPost('khalid', 'Flutter is amazing!'),
+//     ImagePost('omar', 'https://example.com/sunset.png'),
+//     VideoPost('naba', 'https://example.com/snow.mp4'),
+//   ];
+
+//   for (final post in feed) {
+//     post.render();
+//   }
+// }
+
+//Task 5.3
+class Animal {
+  void makeSound() {
+    print('Some generic sound');
   }
 }
 
-class ImagePost extends Post {
-  final String imageUrl;
-  ImagePost(String author, this.imageUrl) : super(author);
-
-  @override
-  void render() {
-    print('Image post by $author at $imageUrl');
+mixin Swimmer {
+  void swim() {
+    print('I can swim!');
   }
 }
 
-class VideoPost extends Post {
-  final String videoUrl;
-  VideoPost(String author, this.videoUrl) : super(author);
-
-  @override
-  void render() {
-    print('Video post by $author at $videoUrl');
-  }
-}
+class Dolphin extends Animal with Swimmer {}
+class Duck extends Animal with Swimmer {}
 
 void main() {
-  List<Post> feed = [
-    TextPost('khalid', 'Flutter is amazing!'),
-    ImagePost('omar', 'https://example.com/sunset.png'),
-    VideoPost('naba', 'https://example.com/snow.mp4'),
-  ];
-
-  for (final post in feed) {
-    post.render();
-  }
+  final dolphin = Dolphin();
+  dolphin.swim();
+  dolphin.makeSound();
+  final duck = Duck();
+  duck.swim();
+  duck.makeSound();
 }
